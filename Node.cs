67563fddd69822f93b69cf6dbdf57b509f9097f8
Riyad <74069878+Riyad-boop@users.noreply.github.com>
2020-11-06 19:19:24 +0000

    Add files via upload
    Script for AI

---
+++ Node.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node 
+{
+    /// <summary>
+    ///  Stores the postion and status of the node. 
+    /// </summary>
+
+
+    public bool walkable;
+    public Vector3 worldPos;
+    public int gridX;
+    public int gridY;
+
+    public int gCost;
+    public int hCost;
+    public Node parent;
+    int heapIndex;
+
+    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
+    {
+        walkable = _walkable;
+        worldPos = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+
+
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+            return heapIndex;
+        }
+        set
+        {
+            heapIndex = value;
+        }
+    }
+
+    public int CompareTo(Node nodeCompare)
+    {
+        int compare = fCost.CompareTo(nodeCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeCompare.hCost);
+        }
+        return -compare;
+    }
+}
